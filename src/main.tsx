25aa6a0ef45afd8079132711cf0e57944be3eba9
Евгений Некрасов <user@MacBook-Pro-user.local>
2025-09-05 21:50:54 +0300

    Сreating basic routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { createRoot } from 'react-dom/client';
-import { App } from './App.tsx';
+import { App } from './app/ui/App/App.tsx';
+import { BrowserRouter } from 'react-router';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-  <>
+  <BrowserRouter>
     <App />
-  </>
+  </BrowserRouter>
 );
